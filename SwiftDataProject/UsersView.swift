669e761d23591a9dd3aa9114f2470e611748012f
Adam Sayer <asayer82@icloud.com>
2024-08-10 08:48:17 +1000

    Completed module: Dynamically sorting and filtering @Query with SwiftUI

---
+++ SwiftDataProject/UsersView.swift
@@ -0,0 +1,18 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Adam Sayer on 10/8/2024.
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersView()
+}
